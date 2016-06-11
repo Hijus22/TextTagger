cfaa79ddf7ff79f4e778dc3c799f81399d1f4560
Antonio <amgarin@my.uri.edu>
2016-06-11 22:57:05 +0200

    tagText implemented
    tagText method implemented,
    the 4 webpages have been tested

--- src/tagger/PageTagger.java
+++ src/tagger/PageTagger.java
@@ -1,11 +1,18 @@
 package tagger;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.MalformedURLException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Date;
 import java.util.logging.Level;
 
-import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
@@ -26,10 +33,18 @@ public class PageTagger {
 	 */
 	public String tagText(String text){
 		String taggedText = null;
-		//TO-DO Use thetagString method of MaxentTagger
+		
+		try{
+			taggedText = tagger.tagString(text);
+		}catch (OutOfMemoryError e){
+			System.err.println("OutOfMemory Error: The sentence is too long");
+			int textLen = text.split("[/. ,:]").length;
+			taggedText = "There is a sentence that is too long, it has "
+					+ "approximately " + textLen + " words";
+		}
 		
 		return taggedText;
-	}
+	}	
 	
 	/**
 	 * Method that takes a java.net.URL [url] as input and returns a String containing
@@ -39,44 +54,75 @@ public class PageTagger {
 	 */
 	public String getText(java.net.URL url){
 		String text = null;
-		
+
 		try(final WebClient webClient = new WebClient();) {
-			System.out.println(url.toString() + " Contains this text in body");
+			//Turns off CSS, Applets and JS
+			webClient.getOptions().setCssEnabled(false);
+			webClient.getOptions().setAppletEnabled(false);
+			webClient.getOptions().setJavaScriptEnabled(false);
 			
 			final HtmlPage startPage = webClient.getPage(url.toString());
-			text = startPage.getBody().asText();
 			
-			System.out.println(text);
+			text = startPage.getBody().asText();			
+			System.out.println("==> TEXT RETRIEVED");
 			
-			
-			
-		} catch (FailingHttpStatusCodeException e) {
-			e.printStackTrace();
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}finally{
+		} catch (Exception e) {
+			// Creates a log file for this exception named with the date of the exception
+			String date = new Date().toString().replace(":", "_");
+			try (PrintWriter writer = new PrintWriter(date + "_error.txt", "UTF-8");){
+				e.printStackTrace(writer);
+			} catch (FileNotFoundException | UnsupportedEncodingException e1) {
+				e1.printStackTrace();
+			}
+		} finally{
 			if (text == null){
 				text = "An error ocurred, couldn't load the page body";
 			}
-		}
-		
+		}	
 		return text;
 	}
 	
 	public static void main(String[] args){
+		String[] urlArray = {
+				"http://gumgum.com/",
+				"http://www.popcrunch.com/jimmy-kimmel-engaged/",
+				"http://gumgum-public.s3.amazonaws.com/numbers.html",
+				"http://www.windingroad.com/articles/reviews/quick-drive-2012-bmw-z4-sdrive28i/"
+				};
+		String text;
+		String taggedText;
 		PageTagger pageTagger = new PageTagger();
-		//Turn off loging just to optimize.
+		Charset cs = Charset.forName("utf-8");
+		Path file = Paths.get("output.txt");
+		
+		//Turn off logging on the WebClient
 		java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
 		
-		System.out.println("-->Requesting text from: ");
-		try {
-			pageTagger.getText(new URL("http://gumgum.com/"));
-		} catch (MalformedURLException e) {
+		try(BufferedWriter writer = Files.newBufferedWriter(file, cs);) {
+			
+			for (String urlText:urlArray){
+				System.out.println("==>Requesting text from: ");
+				writer.write("==>Requesting text from: ");			
+				URL url = new URL(urlText);
+				System.out.println(url.toString() + " Contains this text in body");
+				text = pageTagger.getText(url);
+				
+				System.out.println(url.toString() + " Contains this TAGGED text in body");
+				writer.newLine();
+				writer.write(url.toString() + " Contains this TAGGED text in body");
+				writer.newLine();
+				taggedText = pageTagger.tagText(text);
+				if (taggedText == null) {
+					taggedText = "Text couldn't be tagged";
+				}
+				System.out.println(taggedText);
+				writer.write(taggedText);
+				writer.newLine();
+				writer.newLine();
+			}	
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		//pageTagger.getText(new URL("http://www.popcrunch.com/jimmy-kimmel-engaged/"));
 		
 	}
 }
